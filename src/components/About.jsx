e19b367a3e29ff1b06e8be04d848f001c5760a8e
Ann Shi <annshi204a@gmail.com>
2024-05-21 13:52:56 -0400

    changed max width of about section for better readability

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,10 @@
 function About() {
     return (
       <>
-      <div className="flex flex-col space-y-10 m-5">
+      <div className="max-w-[950px] flex flex-col space-y-10 m-5">
         <div>
             <h5 className="text-3xl font-extrabold pb-2">a little bit about me </h5>
-            <p className="xl:text-2xl">
+            <p className="xl:text-xl">
                 i am an aspiring software engineer who's current interests
                 lie in <span className="font-extrabold">full-stack</span> and <span className="font-extrabold">devops</span>. when i'm
                 not coding, you can probably find me learning
@@ -15,7 +15,7 @@ function About() {
 
         <div>
             <h5 className="text-3xl pb-2 font-extrabold">right now, i'm </h5>
-            <ul className="xl:text-2xl list-disc list-outside marker:text-sm flex flex-col items-start">
+            <ul className="xl:text-xl list-disc list-outside marker:text-sm flex flex-col items-start">
                 <li>writing backend code for an internal tool at RBC as a software developer intern</li>
                 <li>currently developing the front-end for an online game of spyfall as a fun mini side project :)</li>
                 <li>searching for fall 2024 opportunities!</li>
@@ -24,7 +24,7 @@ function About() {
 
         <div>
             <h5 className="text-3xl pb-2 font-extrabold">previously, </h5>
-            <ul className="xl:text-2xl list-disc list-outside marker:text-sm flex flex-col items-start">
+            <ul className="xl:text-xl list-disc list-outside marker:text-sm flex flex-col items-start">
                 <li>worked as a research assistant developer at mcmaster to develop an online assignment submission system for first year cs students</li>
                 <li>provided lessons for over 100+ elementary students across canada to introduce them into the world of coding!</li>
             </ul>
