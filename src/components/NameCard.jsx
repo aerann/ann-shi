05008359cad198c3fa3c2e84fe46fd193564fa1c
Ann Shi <annshi204a@gmail.com>
2024-05-25 19:11:46 -0400

    responsive namecard component

--- src/components/NameCard.jsx
+++ src/components/NameCard.jsx
@@ -3,23 +3,25 @@ import profilePic from '../assets/profile.jpg'
 function NameCard() {
   return (
     <>
-      <div className='relative w-full h-full flex flex-row justify-center'>
+      <div className='w-full h-full flex flex-row justify-center'>
         
-        <div className='relative xs:h-[300px] xs:w-[350px] 2xs:h-[400px] 2xs:w-[450px] sm:h-[400px] sm:w-[600px] md:h-[550px] md:w-[880px] lg:h-[550px] lg:w-[880px] xl:h-[615px] xl:w-[1000px] flex flex-col xs:items-end'>
+        <div className='relative xs:h-[300px] xs:w-[350px] 2xs:h-[400px] 2xs:w-[450px] sm:h-[400px] sm:w-[600px] md:h-[550px] md:w-[880px] lg:h-[550px] lg:w-[880px] xl:h-[615px] xl:w-[1500px] flex flex-col xs:items-center'>
           
           <img 
             src={profilePic} 
             alt="Profile of Ann" 
-            className='absolute bottom-10 shadow-2xl right-[60%] xs:h-[225px] xs:w-[150px] 2xs:h-[375px] 2xs:w-[250px] sm:h-[375px] sm:w-[250px] md:h-[580px] md:w-[380px] lg:h-[600px] lg:w-[424px] xl:h-[608px] xl:w-[424px]' 
+            className='absolute xs:left-[1%] bottom-[20%] sm:left-[10%] sm:bottom-[25%] xl:left-[15%] xl:bottom-[30%] shadow-2xl xs:h-[225px] xs:w-[150px] 2xs:h-[290px] 2xs:w-[200px] sm:h-[325px] sm:w-[220px] md:h-[440px] md:w-[300px] xl:h-[500px] xl:w-[350px]' 
           />
           
-          <p className="font-body xs:text-lg 2xs:text-2xl sm:text-2xl md:text-4xl xl:text-5xl pb-2 sm:pr-20">nice to meet you, i'm</p>
+          <div className="grid grid-cols-2 grid-rows-[35%,65%] box-border w-[85%] pr-9">
+            <p className="font-body xs:text-sm 2xs:text-lg sm:text-2xl md:text-4xl  col-start-2 col-span-1 row-span-1 pb-2">nice to meet you, i'm</p>
+          </div>
 
-          <div className='bg-white-blue bg-opacity-60 box-border h-full w-full shadow-xl grid grid-cols-2 grid-rows-[35%,65%] pr-3 xl:pr-16'>
-            <p className="mt-[-20px] xs:text-4xl 2xs:text-5xl sm:text-5xl md:text-8xl xl:text-9xl font-extrabold col-start-2 col-span-1 row-span-1 self-center">
+          <div className='bg-white-blue bg-opacity-60 box-border h-4/6 w-[85%] shadow-xl grid grid-cols-2 grid-rows-[35%,65%] pr-7 pt-5'>
+            <h5 className="mt-[-20px] xs:text-4xl 2xs:text-4xl sm:text-5xl md:text-8xl font-extrabold col-start-2 col-span-1 row-span-1 self-center">
               ann shi
-            </p>
-            <p className="xs:text-lg 2xs:text-2xl sm:text-2xl md:text-4xl xl:text-5xl col-start-2 row-start-2 col-span-1 row-span-2">
+            </h5>
+            <p className="xs:text-sm 2xs:text-lg sm:text-2xl md:text-4xl col-start-2 row-start-2 col-span-1 row-span-2">
               third year software<br/>
               engineering student <br/> 
               @ mcmaster uni
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,9 +4,9 @@ const Navbar = () => {
       <>
         <nav className='sticky top-0 z-50 backdrop-blur-sm p-4 pr-7 xl:mb-28'>
           <ul className='flex flex-row justify-end items-center gap-10'>
-            <li className='text-xl'><Link to="about" spy={true} smooth={true} offset={-40} duration={500}>about</Link></li>
-            <li className='text-xl'><Link >projects</Link></li>
-            <li className='text-xl'><Link to="contact" spy={true} smooth={true} duration={500}>contact</Link></li>
+            <li className='text-xl cursor-pointer'><Link to="about" spy={true} smooth={true} offset={-60} duration={500}>about</Link></li>
+            <li className='text-xl cursor-pointer'><Link >projects</Link></li>
+            <li className='text-xl cursor-pointer'><Link to="contact" spy={true} smooth={true} duration={500}>contact</Link></li>
           </ul>
         </nav>
       </>
