1397aba34ac9c9bf2f93a20b0a4815ac4050ee6e
Ann Shi <annshi204a@gmail.com>
2024-05-31 11:25:37 -0400

    update spacing

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,10 +7,10 @@ const Home = () => {
         <div className='bg-gradient-to-b from-beige from-0% to-dark-beige to-90%'>
         <Navbar />
             <div className='min-h-screen min-w-0 p-10 xs:px-1 lg:px-20 xl:px-40'>
-            <div className='flex flex-col items-start space-y-60 '>
+            <div className='flex flex-col items-start space-y-20 '>
                 <NameCard />
                 <About/>
-                <div className='w-full flex flex-row xs:justify-center md:justify-end'>
+                <div className='w-full flex flex-row xs:justify-center md:justify-end mt-14'>
                 <Contact />
                 </div>
             </div>
